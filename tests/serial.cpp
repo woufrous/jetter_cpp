ea099c80bdddc7bac5a68409fa6d5dde4e37aa72
Hans Grob <H.Grob@physik.uni-muenchen.de>
2017-10-19 12:06:35 +0200

    tests: unescaping tests added

--- tests/serial.cpp
+++ tests/serial.cpp
@@ -7,22 +7,27 @@
 using namespace jetter;
 using namespace jetter::internal;
 
+std::map<bytestring, bytestring> test_data {
+    {{0xAA}, {0xAA}},
+    {{0xD8}, {JETTER_SCF, 0xC8}},
+    {{0xD9}, {JETTER_SCF, 0xC9}},
+    {{0xDA}, {JETTER_SCF, 0xCA}},
+    {{0xDB}, {JETTER_SCF, 0xCB}},
+    {{0xDC}, {JETTER_SCF, 0xCC}},
+    {{0xDD}, {JETTER_SCF, 0xCD}},
+    {{0xDE}, {JETTER_SCF, 0xCE}},
+    {{0xDF}, {JETTER_SCF, 0xCF}},
+    {{0xDA, 0xDB}, {JETTER_SCF, 0xCA, JETTER_SCF, 0xCB}}
+};
 
-TEST(SerialTest, UnescapeSingle) {
-    std::map<bytestring, bytestring> test_data {
-        {{0xAA}, {0xAA}},
-        {{0xD8}, {JETTER_SCF, 0xC8}},
-        {{0xD9}, {JETTER_SCF, 0xC9}},
-        {{0xDA}, {JETTER_SCF, 0xCA}},
-        {{0xDB}, {JETTER_SCF, 0xCB}},
-        {{0xDC}, {JETTER_SCF, 0xCC}},
-        {{0xDD}, {JETTER_SCF, 0xCD}},
-        {{0xDE}, {JETTER_SCF, 0xCE}},
-        {{0xDF}, {JETTER_SCF, 0xCF}},
-        {{0xDA, 0xDB}, {JETTER_SCF, 0xCA, JETTER_SCF, 0xCB}}
-    };
-
+TEST(SerialTest, Escape) {
     for (const auto& x : test_data) {
         EXPECT_EQ(x.second, escape(x.first));
+    }
+}
+
+TEST(SerialTest, Unescape) {
+    for (const auto& x : test_data) {
+        EXPECT_EQ(x.first, unescape(x.second));
     }
 }
