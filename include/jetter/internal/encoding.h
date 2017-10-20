e4a11fe936e172c0528c646faefc7b1518072403
Hans Grob <H.Grob@physik.uni-muenchen.de>
2017-10-20 18:20:46 +0200

    fmt: use for exception message formatting

--- include/jetter/internal/encoding.h
+++ include/jetter/internal/encoding.h
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <stdexcept>
 
+#include <fmt/format.h>
+
 namespace jetter {
 namespace internal {
 
@@ -54,7 +56,7 @@ bytestring encode<ValueType::INT8>(_CTYPE(ValueType::INT8) value) {
 template<>
 _CTYPE(ValueType::INT8) decode<ValueType::INT8>(const bytestring& data) {
     if (data.size() != 1) {
-        throw std::length_error("One byte reqired");
+        throw std::length_error(fmt::format("One byte reqired; {:d} given", data.size()));
     }
     return static_cast<_CTYPE(ValueType::INT8)>(data[0]);
 }
@@ -68,7 +70,7 @@ bytestring encode<ValueType::UINT8>(_CTYPE(ValueType::UINT8) value) {
 template<>
 _CTYPE(ValueType::UINT8) decode<ValueType::UINT8>(const bytestring& data) {
     if (data.size() != 1) {
-        throw std::length_error("One byte reqired");
+        throw std::length_error(fmt::format("One byte reqired; {:d} given", data.size()));
     }
     return static_cast<_CTYPE(ValueType::UINT8)>(data[0]);
 }
@@ -85,7 +87,7 @@ bytestring encode<ValueType::INT16>(_CTYPE(ValueType::INT16) value) {
 template<>
 _CTYPE(ValueType::INT16) decode<ValueType::INT16>(const bytestring& data) {
     if (data.size() != 2) {
-        throw std::length_error("Two bytes reqired");
+        throw std::length_error(fmt::format("Two bytes reqired; {:d} given", data.size()));
     }
     return static_cast<_CTYPE(ValueType::INT16)>(
         data[0] << 8 |
@@ -105,7 +107,7 @@ bytestring encode<ValueType::UINT16>(_CTYPE(ValueType::UINT16) value) {
 template<>
 _CTYPE(ValueType::UINT16) decode<ValueType::UINT16>(const bytestring& data) {
     if (data.size() != 2) {
-        throw std::length_error("Two bytes reqired");
+        throw std::length_error(fmt::format("Two bytes reqired; {:d} given", data.size()));
     }
     return static_cast<_CTYPE(ValueType::UINT16)>(
         data[0] << 8 |
@@ -126,7 +128,7 @@ bytestring encode<ValueType::INT24>(_CTYPE(ValueType::INT24) value) {
 template<>
 _CTYPE(ValueType::INT24) decode<ValueType::INT24>(const bytestring& data) {
     if (data.size() != 3) {
-        throw std::length_error("Three bytes reqired");
+        throw std::length_error(fmt::format("Three bytes reqired; {:d} given", data.size()));
     }
     // This is a little tricky, since int32_t requires bit 31 to be the sign bit.
     // To work around that, we have to shift 8bits too far,  convert and then shift back.
@@ -150,7 +152,7 @@ bytestring encode<ValueType::UINT24>(_CTYPE(ValueType::UINT24) value) {
 template<>
 _CTYPE(ValueType::UINT24) decode<ValueType::UINT24>(const bytestring& data) {
     if (data.size() != 3) {
-        throw std::length_error("Three bytes reqired");
+        throw std::length_error(fmt::format("Three bytes reqired; {:d} given", data.size()));
     }
     return static_cast<_CTYPE(ValueType::UINT24)>(
         data[0] << 16 |
