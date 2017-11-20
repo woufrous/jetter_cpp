a00d9e33680e87ebc5362a8f0bddd745707dbb7d
Hans Grob <H.Grob@physik.uni-muenchen.de>
2017-11-20 20:50:09 +0100

    JetterDev: more implementation code

--- include/jetter/JetterDevice.h
+++ include/jetter/JetterDevice.h
@@ -22,6 +22,12 @@ enum StatusBit {
     NoError,
     Reception,
 };
+
+enum ProgramState {
+    Restart = 1,
+    Stop,
+    Continue,
+};
 } // namespace internal
 
 
@@ -59,17 +65,17 @@ class IJetterDevice {
         virtual ~IJetterDevice() {}
 
         // program control
-        virtual void continue_program() const = 0;
-        virtual void stop_program() const = 0;
-        virtual void restart_program() const = 0;
+        virtual void continue_program(uint8_t task) const = 0;
+        virtual void stop_program(uint8_t task) const = 0;
+        virtual void restart_program(uint8_t task) const = 0;
         // register control
-        virtual std::vector<reg_t> get_register_block(uint32_t reg_base, uint32_t count) const = 0;
+        virtual std::vector<reg_t> get_register_block(addr_t reg_base, size_t count) const = 0;
         virtual reg_t get_register(addr_t reg) const = 0;
         virtual void set_register(addr_t reg, reg_t value) const = 0;
         // flag control
-        virtual bool get_flag(addr_t flag) const = 0; 
-        virtual void set_flag(addr_t flag) const = 0; 
-        virtual void reset_flag(addr_t flag) const = 0; 
+        virtual bool get_flag(uint16_t flag) const = 0;
+        virtual void set_flag(uint16_t flag) const = 0;
+        virtual void reset_flag(uint16_t flag) const = 0;
 };
 
 
@@ -81,22 +87,24 @@ class JetterDevice : public IJetterDevice {
             : JetterDevice(std::make_unique<jetter::JetterCom>(port_name)) {}
 
         // program control
-        void continue_program() const override;
-        void stop_program() const override;
-        void restart_program() const override;
+        void continue_program(uint8_t task) const override;
+        void stop_program(uint8_t task) const override;
+        void restart_program(uint8_t task) const override;
         // register control
-        std::vector<reg_t> get_register_block(uint32_t reg_base, uint32_t count) const override;
+        std::vector<reg_t> get_register_block(addr_t reg_base, size_t count) const override;
         reg_t get_register(addr_t reg) const override;
         void set_register(addr_t reg, reg_t value) const override;
         // flag control
-        bool get_flag(addr_t flag) const override; 
-        void set_flag(addr_t flag) const override; 
-        void reset_flag(addr_t flag) const override; 
+        bool get_flag(uint16_t flag) const override;
+        void set_flag(uint16_t flag) const override;
+        void reset_flag(uint16_t flag) const override;
 
     private:
         std::unique_ptr<jetter::JetterCom> dev_;
 
         bytestring sync_command(const bytestring& data) const;
+        void set_program_state(uint8_t program, internal::ProgramState state) const;
+        std::vector<reg_t> get_register_block_impl_(addr_t reg_base, uint8_t count, uint8_t mod) const;
 };
 
 }
--- src/JetterDevice.cpp
+++ src/JetterDevice.cpp
@@ -1,5 +1,3 @@
-#include <memory>
-
 #include <jetter/JetterDevice.h>
 #include <jetter/internal/encoding.h>
 
@@ -13,28 +11,92 @@ bytestring JetterDevice::sync_command(const bytestring& data) const {
     return resp.substr(1);
 }
 
-void JetterDevice::continue_program() const {}
-void JetterDevice::stop_program() const {}
-void JetterDevice::restart_program() const {}
-std::vector<reg_t> JetterDevice::get_register_block(uint32_t reg_base, uint32_t count) const {
-    return std::vector<reg_t>({0});
+void JetterDevice::set_program_state(uint8_t task, internal::ProgramState state) const {
+    if (task > 0xAA) {
+        throw JetterException(1<<internal::StatusBit::InvalidParam);
+    }
+    bytestring req = {'N'};
+    req += internal::encode<internal::ValueType::UINT8>(static_cast<uint8_t>(state));
+    req += internal::encode<internal::ValueType::UINT8>(task);
+
+    sync_command(req);
 }
+
+void JetterDevice::continue_program(uint8_t task) const {
+    set_program_state(task, internal::ProgramState::Continue);
+}
+
+void JetterDevice::stop_program(uint8_t task) const {
+    set_program_state(task, internal::ProgramState::Stop);
+}
+
+void JetterDevice::restart_program(uint8_t task) const {
+    set_program_state(task, internal::ProgramState::Restart);
+}
+
+std::vector<reg_t>
+JetterDevice::get_register_block_impl_(addr_t reg_base, uint8_t count, uint8_t mod) const {
+    std::vector<reg_t> ret(32);
+    bytestring req = {'h'};
+    req += internal::encode<internal::ValueType::UINT8>(mod);
+    req += internal::encode<internal::ValueType::UINT8>(count);
+    req += internal::encode<internal::ValueType::UINT24>(reg_base);
+
+    auto resp = sync_command(req);
+    reg_t tmp;
+    for (size_t i=0; i<resp.length(); i+=3) {
+        tmp = internal::decode<internal::ValueType::INT24>(resp.substr(i));
+        ret.push_back(tmp);
+    }
+    return ret;
+}
+
+std::vector<reg_t> JetterDevice::get_register_block(addr_t reg_base, size_t count) const {
+    std::vector<reg_t> ret(count);
+    while (count > 0) {
+        auto tmp = get_register_block_impl_(reg_base, count < 32 ? count : 32, 0x01);
+        ret.insert(ret.end(), tmp.begin(), tmp.end());
+        count -= tmp.size();
+    }
+    return ret;
+}
+
 reg_t JetterDevice::get_register(addr_t reg) const {
     bytestring req = {'Q'};
-    /*
     req += internal::encode<internal::ValueType::UINT24>(reg);
-    auto ret = sync_command(req);
-    if (ret.first) {
-        return internal::decode<internal::ValueType::INT24>(ret.second);
-    }
-    */
-    return 0;
+
+    auto resp = sync_command(req);
+    return internal::decode<internal::ValueType::INT24>(resp);
 }
-void JetterDevice::set_register(addr_t reg, reg_t value) const {}
-bool JetterDevice::get_flag(addr_t flag) const {
-    return false;
-} 
-void JetterDevice::set_flag(addr_t flag) const {} 
-void JetterDevice::reset_flag(addr_t flag) const {} 
+
+void JetterDevice::set_register(addr_t reg, reg_t value) const {
+    bytestring req = {'R'};
+    req += internal::encode<internal::ValueType::UINT24>(reg);
+    req += internal::encode<internal::ValueType::INT24>(value);
+
+    sync_command(req);
+}
+
+bool JetterDevice::get_flag(uint16_t flag) const {
+    bytestring req = {'K'};
+    req += internal::encode<internal::ValueType::UINT16>(flag);
+
+    auto resp = sync_command(req);
+    return resp[0] & (1<<internal::StatusBit::Data);
+}
+
+void JetterDevice::set_flag(uint16_t flag) const {
+    bytestring req = {'L'};
+    req += internal::encode<internal::ValueType::UINT16>(flag);
+
+    sync_command(req);
+}
+
+void JetterDevice::reset_flag(uint16_t flag) const {
+    bytestring req = {'M'};
+    req += internal::encode<internal::ValueType::UINT16>(flag);
+
+    sync_command(req);
+}
 
 }
